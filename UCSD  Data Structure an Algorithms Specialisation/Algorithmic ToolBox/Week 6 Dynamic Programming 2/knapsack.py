964bae51755d272004cbac4a07aba18787307d8e
simon lan <simonlan0930@gmail.com>
2024-07-09 13:53:44 +1000

    Add documentation for knapsack problem without repetition , partially complete partition subsets problem

--- UCSD  Data Structure an Algorithms Specialisation/Algorithmic ToolBox/Week 6 Dynamic Programming 2/knapsack.py
+++ UCSD  Data Structure an Algorithms Specialisation/Algorithmic ToolBox/Week 6 Dynamic Programming 2/knapsack.py
@@ -1,6 +1,17 @@
 from sys import stdin
 
 
+'''
+1 : create a 2-D array , i * w 
+    i : the number of items available to select
+    w : the capacity of knapsack 
+    
+    Fill the role with 0 ,  where i = 0 , w (0 - capacity)
+    Fill the role with 0 ,  where w = 0 , i (0 - number of weights)
+    
+2 : for the number of items available to select from (1 : len(weight+1), 
+    
+'''
 def maximum_gold(capacity, weights):
     dp_array = [[0 for i in range(capacity+1)] for j in range(len(weights)+1)]
 
@@ -8,6 +19,8 @@ def maximum_gold(capacity, weights):
         for w in range(1, capacity+1):
 
             dp_array[i][w] = dp_array[i - 1][w]
+
+
 
             if w >= weights[i-1]:
 
--- UCSD  Data Structure an Algorithms Specialisation/Algorithmic ToolBox/Week 6 Dynamic Programming 2/partition3.py
+++ UCSD  Data Structure an Algorithms Specialisation/Algorithmic ToolBox/Week 6 Dynamic Programming 2/partition3.py
@@ -1,11 +1,43 @@
 from sys import stdin
 
+'''
+3 Partition Problem :
+    An equal subset dp_arrayition problem  determines whether  a given set can be partitioned
+    into 3  subsets such that the sum of elements in all subsets are the same 
+    
+    
+    1. If Sum (total) is not divisible by 3, we can’t divide the array into three subsets with an equal sum. 
+'''
+
 
 def partition3(values):
-    return 0
+    if len(values) < 3 or sum(values) % 3 != 0:
+        return 0
+
+    subset_sum = sum(values) // 3
+
+    dp_array = [[[0 for i in range(subset_sum+1)] for i in range(len(values) + 1)] for i in range(len(values)+ 1)]
+
+    # zero sum
+
+    for i in range(subset_sum + 1):
+        for j in range(subset_sum + 1):
+            dp_array[0][i][j] = 1
+
+    for i in range(1, len(values) + 1):
+
+        num = values[i - 1]
+
+        for s_1 in range(subset_sum + 1):
+            for s_2 in range(subset_sum + 1):
+                pass
+
+    return (subset_sum, len(dp_array), len(dp_array[0]), len(dp_array[0][0]), dp_array)
 
 
 if __name__ == '__main__':
     input_n, *input_values = list(map(int, stdin.read().split()))
     assert input_n == len(input_values)
     print(partition3(input_values))
+
+    # 13
